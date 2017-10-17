dc9051ccfc15289bf1ebbdcdcbfebefba3495458
nicolasParodi <mnicolasparodi@gmail.com>
2017-10-17 11:45:44 -0300

    Start Project

---
+++ sushiCat/Assets/CatManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatManager : MonoBehaviour {
+
+    public GameObject CatBall;
+    private bool isTrow = false;
+   
+
+    public void spawnCat()
+    {
+        if (!isTrow)
+        {
+            if (Input.GetAxis("Fire1") == 1)
+            {
+
+                isTrow = true;
+            }
+
+
+        }
+    }
+}
---
+++ sushiCat/Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    
+	public static GameManager instance;
+
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this.gameObject);
+    }
+
+    
+
+
+
+}
